be7032810f6766ea107d3adfa96510df5d3957ff
ahmadullahnikzad <ahmadullahnikzad1919@gmail.com>
2020-08-18 13:46:12 +0430

    project2

--- project1.py
+++ project1.py
@@ -1,2 +1,11 @@
-for i in range(5):
-    print(i)
+#finding factorial of a number
+fact=int(input('enter your number'))
+sum=1
+for i in range(1,fact):
+    if fact<0:
+        sum='negative number doesn\'t have factorial'
+    elif fact==0:
+        sum=1
+    else:
+        sum*=i
+print(sum)
