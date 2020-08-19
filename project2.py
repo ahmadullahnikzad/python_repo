afd3e42456c917257f8b3569621dd7eff44843c5
ahmadullahnikzad <ahmadullahnikzad1919@gmail.com>
2020-08-19 06:06:07 +0430

    tuple

--- project2.py
+++ project2.py
@@ -1,12 +1,2 @@
-electri_charge=input()
-spin=input()
-if electri_charge == '1/2' and spin == '-1/3':
-    print('Strange Quark')
-elif electri_charge == '2/3' and spin == '1/2':
-    print('Charm Quark')
-elif electri_charge == '-1' and spin == '1/2':
-    print('Electron Lepton')
-elif electri_charge == '0' and spin == '1/2':
-    print('Neutrino Lepton')
-elif electri_charge == '0' and spin == '1':
-    print('Photo Boson')
+weekend='Saturday','Sunday'
+print(type(weekend))
