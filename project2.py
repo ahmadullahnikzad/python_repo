1875d0ad1dd09643cd20982edaab4c0ff5a82937
ahmadullahnikzad <ahmadullahnikzad1919@gmail.com>
2020-08-19 05:42:19 +0430

    readme

--- project2.py
+++ project2.py
@@ -1,1 +1,12 @@
-print('hello')
+electri_charge=input()
+spin=input()
+if electri_charge == '1/2' and spin == '-1/3':
+    print('Strange Quark')
+elif electri_charge == '2/3' and spin == '1/2':
+    print('Charm Quark')  
+elif electri_charge == '-1' and spin == '1/2':
+    print('Electron Lepton')
+elif electri_charge == '0' and spin == '1/2':
+    print('Neutrino Lepton')
+elif electri_charge == '0' and spin == '1':
+    print('Photo Boson')
