94b5ec2fc4b6ce71bebc21544951d74b5c2f8f4a
ahmadullahnikzad <ahmadullahnikzad1919@gmail.com>
2020-08-19 06:01:10 +0430

    electric

--- project2.py
+++ project2.py
@@ -3,7 +3,7 @@ spin=input()
 if electri_charge == '1/2' and spin == '-1/3':
     print('Strange Quark')
 elif electri_charge == '2/3' and spin == '1/2':
-    print('Charm Quark')  
+    print('Charm Quark')
 elif electri_charge == '-1' and spin == '1/2':
     print('Electron Lepton')
 elif electri_charge == '0' and spin == '1/2':
